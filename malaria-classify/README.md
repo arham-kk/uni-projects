fb4c20ce90c997da00c473d614f90ddb6683996e
arham-kk <kmuhammadarham@gmail.com>
2023-11-18 02:35:47 +0500

    update

--- malaria-classify/README.md
+++ malaria-classify/README.md
@@ -1,52 +1,48 @@
 # Malaria Classifier
-This Python script provides a GUI application to classify an image as infected or uninfected with malaria using a trained machine learning model. The application allows the user to select an image file in JPEG, JPG, or PNG format and displays the classification result and the image.
 
+This Python application uses a trained deep learning model to classify whether an input image is infected or uninfected with malaria. The graphical user interface (GUI) is built using the Tkinter library, and image processing is facilitated by the PIL (Pillow) library. The deep learning model is implemented using TensorFlow and Keras.
 
 https://user-images.githubusercontent.com/108623726/222744577-d9933cc4-0c56-442b-a07e-85eed418a90d.mp4
 
+## Installation
 
-## Prerequisites
-This application requires the following Python packages to be installed:
+1. Clone the repository or download the `app.py` file.
+2. Ensure you have Python 3.x installed.
+3. Install the required dependencies using the following command:
 
-- tkinter
-- PIL
-- numpy
-- tensorflow
+```bash
+pip install -r requirements.txt
+```
 
-The script also requires a trained machine learning model in the form of an h5 file to be stored in the same directory with the name model.h5.
+## Usage
 
-## How to use
-To use the application, run the script in a Python environment. A GUI window will open, and the user can follow these steps:
+1. Ensure you have a pre-trained model file named `model.h5` in the same directory as the script.
+2. Run the script:
 
-Click the "Open Image" button and select an image file in JPEG, JPG, or PNG format from the file dialog box.
-Wait for the classification result to appear in the label below the button.
-The image will also be displayed on the window with the size of 200x200 pixels.
-Note: The application will only classify an image as infected or uninfected with malaria if the file format is supported, and the image file is loaded successfully. If there is any error, an error message box will appear.
+```bash
+python app.py
+```
 
-## GUI Elements
-- Title:
+3. The GUI window will appear with the title "Malaria Disease Classifier."
+4. Click the "Open Image" button to choose an image for classification. Supported formats include `.jpeg`, `.jpg`, and `.png`.
+5. The result of the classification (INFECTED or UNINFECTED) will be displayed, along with a resized version of the uploaded image.
 
-  The title of the application is displayed at the top of the window with the font size of 28 and bold.
+## Dependencies
 
-- Subtitle:
-
-  The subtitle of the application is displayed below the title, providing a brief description of the application's purpose with the font size of 12.
-
-- Open Image Button:
-
-  This button allows the user to open a file dialog box to select an image file. The button is labeled "Open Image," with the font size of 12 and bold.
-
-- Result Label:
-
-  This label displays the classification result of the selected image file as "INFECTED" or "UNINFECTED" with the font size of 18.
-
-- Image Label:
-
-  This label displays the selected image with the size of 200x200 pixels.
+- Python 3.x
+- Tkinter
+- Pillow (PIL)
+- NumPy
+- TensorFlow
+- Keras
 
 ## Classification Models
 - [Models](https://github.com/arham-kk/malaria-detection-models)
 
-#3 Credits
+## Credits
 
 The machine learning model was trained using the [Malaria Cell Images Dataset](https://www.kaggle.com/datasets/iarunava/cell-images-for-detecting-malaria) on Kaggle.
+
+## License
+
+This project is licensed under the [License Name] License - see the [LICENSE](LICENSE) file for details.
--- malaria-classify/app.py
+++ malaria-classify/app.py
@@ -2,94 +2,92 @@ import tkinter as tk
 from PIL import Image, ImageTk
 from tkinter import filedialog, messagebox
 import numpy as np
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
+import tensorflow as tf
+from keras.models import load_model
+from keras.preprocessing import image
 
-# Load the model and handle any errors
-try:
-    model = load_model('model.h5')
-except:
-    messagebox.showerror("Error", "Failed to load model")
+class malaria:
+    def __init__(self):
+        # Load the model
+        try:
+            self.model = load_model('model.h5')
+        except Exception as e:
+            messagebox.showerror("Error", f"Failed to load model: {str(e)}")
 
-def model_predict(img_path, model):
-    # Load the image and handle any errors
-    try:
-        img = image.load_img(img_path, target_size=(50, 50))
-    except:
-        messagebox.showerror("Error", "Failed to load image")
+        # Set up the GUI
+        self.root = tk.Tk()
+        self.root.title("Malaria Disease Classifier")
+        self.root.configure(bg='#ffffff')
+        self.root.geometry("600x500")
+        self.root.resizable(False, False)
 
-    # Preprocess the image
-    x = image.img_to_array(img) / 255
-    x = np.expand_dims(x, axis=0)
+        # Set up fonts
+        self.title_font = ("Helvetica", 28, "bold")
+        self.subtitle_font = ("Helvetica", 12)
+        self.label_font = ("Helvetica", 18)
+        self.button_font = ("Helvetica", 12, "bold")
+        self.footer_font = ("Helvetica", 8)
 
-    # Make predictions
-    preds = model.predict(x)
-    preds = np.argmax(preds, axis=1)
+        # Set up colors
+        self.primary_color = "#3f51b5"
+        self.secondary_color = "#ffffff"
+        self.highlight_color = "#757de8"
+        self.active_color = "#1a237e"
+        self.footer_color = "#808080"
 
-    # Return the prediction
-    return "INFECTED" if preds[0] == 1 else "UNINFECTED"
+        self.padx = 10
+        self.pady = 10
 
-def open_file():
-    file_path = filedialog.askopenfilename()
-    if file_path and file_path.endswith(('.jpeg', '.jpg', '.png', '.JPEG', '.JPG', '.PNG')):
-        prediction = model_predict(file_path, model)
-        result_label.config(text=prediction)
+        self.title_label = tk.Label(self.root, text="Malaria Disease Classifier", font=self.title_font, bg=self.primary_color, fg=self.secondary_color, padx=self.padx, pady=self.pady)
+        self.title_label.pack(fill=tk.X)
 
-        img = Image.open(file_path)
-        img = img.resize((200, 200), resample=Image.LANCZOS)
-        img = ImageTk.PhotoImage(img)
-        image_label.config(image=img)
-        image_label.image = img
-    else:
-        messagebox.showerror("Error", "File format not supported or no file selected. Please select a .jpeg, .jpg or .png image")
+        self.subtitle_label = tk.Label(self.root, text="Upload an image to classify it as infected or uninfected with malaria", font=self.subtitle_font, bg=self.secondary_color, fg=self.primary_color, padx=self.padx, pady=self.pady)
+        self.subtitle_label.pack(fill=tk.X)
 
-# Set up the GUI
-root = tk.Tk()
-root.title("Malaria Disease Classifier")
-root.configure(bg='#ffffff')
-root.geometry("600x500")
-root.resizable(False, False)
+        self.file_button = tk.Button(self.root, text="Open Image", font=self.button_font, fg=self.secondary_color, bg=self.primary_color, activebackground=self.active_color, activeforeground=self.secondary_color, bd=0, command=self.open_file)
+        self.file_button.pack(padx=self.padx, pady=self.pady)
 
-# Set up fonts
-title_font = ("Helvetica", 28, "bold")
-subtitle_font = ("Helvetica", 12)
-label_font = ("Helvetica", 18)
-button_font = ("Helvetica", 12, "bold")
-footer_font = ("Helvetica", 8)
+        self.result_label = tk.Label(self.root, text="", font=self.label_font, bg=self.secondary_color, fg=self.primary_color, padx=self.padx, pady=self.pady)
+        self.result_label.pack(fill=tk.X, padx=self.padx, pady=self.pady)
 
-# Set up colors
-primary_color = "#3f51b5"
-secondary_color = "#ffffff"
-highlight_color = "#757de8"
-active_color = "#1a237e"
-footer_color = "#808080"
+        self.image_label = tk.Label(self.root, text="", font=self.label_font, bg=self.secondary_color, fg=self.primary_color, padx=self.padx, pady=self.pady)
+        self.image_label.pack(fill=tk.BOTH, expand=True, padx=self.padx, pady=self.pady)
 
-# Set up padding
-padx = 10
-pady = 10
+        self.footer_label = tk.Label(self.root, text="©2023 Malaria Disease Classifier. All rights reserved.", font=self.footer_font, bg=self.secondary_color, fg=self.footer_color, padx=self.padx, pady=self.pady)
+        self.footer_label.pack(side=tk.BOTTOM, fill=tk.X)
 
-# Set up the title
-title_label = tk.Label(root, text="Malaria Disease Classifier", font=title_font, bg=primary_color, fg=secondary_color, padx=padx, pady=pady)
-title_label.pack(fill=tk.X)
+        self.root.mainloop()
 
-# Set up the subtitle
-subtitle_label = tk.Label(root, text="Upload an image to classify it as infected or uninfected with malaria", font=subtitle_font, bg=secondary_color, fg=primary_color, padx=padx, pady=pady)
-subtitle_label.pack(fill=tk.X)
+    def model_predict(self, img_path):
+        # Load the image
+        try:
+            img = image.load_img(img_path, target_size=(50, 50))
+        except Exception as e:
+            messagebox.showerror("Error", f"Failed to load image: {str(e)}")
 
-# Set up the file button
-file_button = tk.Button(root, text="Open Image", font=button_font, fg=secondary_color, bg=primary_color, activebackground=active_color, activeforeground=secondary_color, bd=0, command=open_file)
-file_button.pack(padx=padx, pady=pady)
+        # Preprocess the image
+        x = image.img_to_array(img) / 255
+        x = np.expand_dims(x, axis=0)
 
-# Set up the result label
-result_label = tk.Label(root, text="", font=label_font, bg=secondary_color, fg=primary_color, padx=padx, pady=pady)
-result_label.pack(fill=tk.X, padx=padx, pady=pady)
+        # Make predictions
+        preds = self.model.predict(x)
+        preds = np.argmax(preds, axis=1)
 
-# Set up the image label
-image_label = tk.Label(root, text="", font=label_font, bg=secondary_color, fg=primary_color, padx=padx, pady=pady)
-image_label.pack(fill=tk.BOTH, expand=True, padx=padx, pady=pady)
+        return "INFECTED" if preds[0] == 1 else "UNINFECTED"
 
-# Set up the footer
-footer_label = tk.Label(root, text="©2023 Malaria Disease Classifier. All rights reserved.", font=footer_font, bg=secondary_color, fg=footer_color, padx=padx, pady=pady)
-footer_label.pack(side=tk.BOTTOM, fill=tk.X)
+    def open_file(self):
+        file_path = filedialog.askopenfilename()
+        if file_path and file_path.endswith(('.jpeg', '.jpg', '.png', '.JPEG', '.JPG', '.PNG')):
+            prediction = self.model_predict(file_path)
+            self.result_label.config(text=prediction)
 
-root.mainloop()
+            img = Image.open(file_path)
+            img = img.resize((200, 200), resample=Image.LANCZOS)
+            img = ImageTk.PhotoImage(img)
+            self.image_label.config(image=img)
+            self.image_label.image = img
+        else:
+            messagebox.showerror("Error", "File format not supported or no file selected. Please select a .jpeg, .jpg or .png image")
+
+if __name__ == "__main__":
+    malaria()
